57a4b5d1808d63d5d5f47c35a17f26cb77fe814a
Sakshi <sakshishetty1231997@gmail.com>
2021-09-04 06:51:12 +0530

    Added: Method to find the cheapest hotel in the list

--- src/main/java/com/bridgelabz/HotelReservationSystem.java
+++ src/main/java/com/bridgelabz/HotelReservationSystem.java
@@ -21,5 +21,19 @@ public class HotelReservationSystem {
     public boolean addHotel(Hotel hotel) {
         return this.hotels.add(hotel);
     }
+
+
+    /**
+     * This method is used to find the cheapest hotel in the HotelList
+     * @return - cheapestHotel - The cheapest hotel in the list
+     */
+    public int findCheapestHotel(){
+        int LakeWoodRate = 110; int BridgeWoodRate = 160; int Ridgewood = 220;
+        int cheapestHotel = (LakeWoodRate>BridgeWoodRate) ?
+                (LakeWoodRate>Ridgewood ? LakeWoodRate : Ridgewood) :
+                (BridgeWoodRate>Ridgewood ? BridgeWoodRate : Ridgewood) ;
+        System.out.println("The cheapest Hotel is " +cheapestHotel);
+        return cheapestHotel;
+    }
 }
 
--- src/test/java/com/bridgelabz/test/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/test/HotelReservationTest.java
@@ -5,12 +5,18 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 public class HotelReservationTest {
-   HotelReservationSystem hotelReservation = new HotelReservationSystem();
+    HotelReservationSystem hotelReservation = new HotelReservationSystem();
 
     @Test
     public void whenAddedHotel_ShouldReturn_AddedHotel() {
-        Assert.assertTrue(hotelReservation.addHotel( new Hotel("LakeWood",110)));
-        Assert.assertTrue(hotelReservation.addHotel( new Hotel("Bridgewood",160)));
-        Assert.assertTrue(hotelReservation.addHotel( new Hotel("Ridgewood",220)));
+        Assert.assertTrue(hotelReservation.addHotel(new Hotel("LakeWood", 110)));
+        Assert.assertTrue(hotelReservation.addHotel(new Hotel("Bridgewood", 160)));
+        Assert.assertTrue(hotelReservation.addHotel(new Hotel("Ridgewood", 220)));
+    }
+
+    @Test
+    public void toFindTheChepeastHotel() {
+        HotelReservationSystem cheapHotel = new HotelReservationSystem();
+        cheapHotel.findCheapestHotel();
     }
 }
