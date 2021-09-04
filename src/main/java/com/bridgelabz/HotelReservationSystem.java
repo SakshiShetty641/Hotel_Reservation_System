ce064402a1b94fa9f4175356ba85b80a87ef2889
Sakshi <sakshishetty1231997@gmail.com>
2021-09-04 06:33:51 +0530

    Added: Welcome Message to the Program

---
+++ src/main/java/com/bridgelabz/HotelReservationSystem.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+/**
+ * Purpose - To build a Hotel Reservation System
+ * Author - Sakshi Shetty
+ * @version - 16.0
+ * @since - 2021-09-02
+ */
+public class HotelReservationSystem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the program");
+    }
+}
